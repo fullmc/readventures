5ee658167b56f1dfd5138329555f3390f66cf93b
MC <marie-claire.lambo@edu.devinci.fr>
2025-11-14 21:17:05 +0100

    ✨ feat: protected routes and add logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useEffect } from 'react'
-import { AuthProvider, useAuth } from './context/AuthContext'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-// import Profile from './pages/Profile'
-type ProtectedRouteProps = {
-  component: React.ComponentType<any>;
-};
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+
   return (
     <AuthProvider>
       <Router>
         <div className="App">
+   
           <Routes>
+
+            {/* Public */}
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/home" element={<ProtectedRoute component={Home} />} />
-            <Route path="/" element={<Navigate replace to="/login" />} />
+
+            {/* Protected routes */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+
+            {/* Default redirection */}
+            <Route path="/" element={<Navigate replace to="/home" />} />
+
           </Routes>
         </div>
       </Router>
@@ -26,14 +34,4 @@ function App() {
   );
 }
 
-function ProtectedRoute({ component: Component }: ProtectedRouteProps) {
-  const { authToken } = useAuth();
-
-  useEffect(() => {
-    console.log("Auth token has changed, current token:", authToken);
-  }, [authToken]);
-
-  return authToken ? <Component /> : <Navigate to="/login" replace />;
-}
 export default App;
-
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
+import { useAuth } from '../context/AuthContext';
+
+
 function Home() {
+  const { logout } = useAuth()
+
   return (
     <div>
       <h2>Accueil</h2>
       <p>Bienvenue dans ta librairie personnelle 📚</p>
+      <button onClick={logout}>Se déconnecter</button>
     </div>
+
   )
 }
 
